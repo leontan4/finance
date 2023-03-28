612ca231b5aab25539dc8570b3310c7c714c18f4
zenTL <leon.tan004@gmail.com>
2023-03-27 20:40:58 -0500

    updated app

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-import os
-import json
-import ast
-
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session, jsonify, url_for
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
-
 from helpers import apology, login_required, lookup, usd
+
+import os
+import json
+import ast
+
 
 # Configure application
 app = Flask(__name__)
